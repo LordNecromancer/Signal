2c8eae7fd17a5ea444c078ca4ac9b0aba5dc9bf3
M.M Danesh Pajouh <37251185+LordNecromancer@users.noreply.github.com>
2025-06-30 03:54:21 +0330

    Update HeaderDropDown.js

--- components/HeaderDropDown.js
+++ components/HeaderDropDown.js
@@ -10,7 +10,7 @@ import { useTheme } from '../context/ThemeContext';
 export default function HeaderDropdown() {
   const [dropdownVisible, setDropdownVisible] = useState(false);
   const [themeMenuVisible, setThemeMenuVisible] = useState(false);
-  const { toggleTheme } = useTheme();Add commentMore actions
+  const { toggleTheme } = useTheme();
   const [theme, setTheme] = useState('light'); // Default theme
 
   const toggleTheme = (selectedTheme) => setTheme(selectedTheme);
