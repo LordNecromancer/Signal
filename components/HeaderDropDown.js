71a0d1b6741c04f3f11487f634fd4e4029ace854
M.M Danesh Pajouh <37251185+LordNecromancer@users.noreply.github.com>
2025-06-30 03:43:50 +0330

    Update HeaderDropDown.js

--- components/HeaderDropDown.js
+++ components/HeaderDropDown.js
@@ -4,11 +4,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Ionicons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
 import { supabase } from '../app/supabase';
+import { useTheme } from '../context/ThemeContext';
 
 
 export default function HeaderDropdown() {
   const [dropdownVisible, setDropdownVisible] = useState(false);
   const [themeMenuVisible, setThemeMenuVisible] = useState(false);
+  const { toggleTheme } = useTheme();Add commentMore actions
   const [theme, setTheme] = useState('light'); // Default theme
 
   const toggleTheme = (selectedTheme) => setTheme(selectedTheme);
