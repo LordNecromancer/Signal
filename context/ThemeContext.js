b39a4acbcc96e16fb93af32dd007503a23799024
M.M Danesh Pajouh <37251185+LordNecromancer@users.noreply.github.com>
2025-07-01 19:11:43 +0330

    Update ThemeContext.js

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -2,6 +2,9 @@ import React, { createContext, useState, useContext } from 'react';
 
 const ThemeContext = createContext('light');
 
+/*
+Theme context to propagate theme information from HeaderDropDown component to main page.
+*/
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState('light'); // Default theme
 
